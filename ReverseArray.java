ed5f06e9ef66d9de6f993c19b905e49b99b71775
Shashwat Tiwari <shashwattiwari8@gmail.com>
2021-02-28 20:00:00 -0600

    Reverse Array HackerRank

---
+++ ReverseArray.java
@@ -0,0 +1,9 @@
+static int[] reverseArray(int[] a) {
+        for(int i=0;i<a.length/2;i++)
+        {
+            int temp=a[i];
+            a[i]=a[a.length-1-i];
+            a[a.length-1-i]=temp;
+        }
+        return a;
+    }
