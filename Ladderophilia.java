9e92b17e35cdf787abb6e59a274c90d6754236b8
Shashwat Tiwari <shashwattiwari8@gmail.com>
2021-03-19 00:43:31 +0530

    Ladderophilia HackerEarth

---
+++ Ladderophilia.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class TestClass
+{
+    public static void main(String args[])
+    {
+        Scanner obj = new Scanner(System.in);
+        int t=obj.nextInt();
+        while(t>0)
+        {
+            System.out.println("*"+"   "+"*");
+            System.out.println("*"+"   "+"*");
+            System.out.println("*****");
+            t--;
+        }
+        System.out.println("*"+"   "+"*");
+                    System.out.println("*"+"   "+"*");
+    }
+}
