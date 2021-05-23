e2dad3e1be7f46a4bad1e886a1fd91ff13bec5a0
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2021-05-23 08:27:25 +0530

    Create NumberOfGoodPairs.java

---
+++ NumberOfGoodPairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(map.containsKey(nums[i]))
+            {
+                sum+=map.get(nums[i]);
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else
+            {
+                map.put(nums[i],1);
+            }
+        }
+        return sum;
+    }
+}
