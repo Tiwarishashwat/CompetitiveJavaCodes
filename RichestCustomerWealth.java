0beb6dd191f39ccfad61ee98eccdd9842ad7918c
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2021-05-23 08:12:06 +0530

    Create RichestCustomerWealth.java

---
+++ RichestCustomerWealth.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int sum = 0;
+        int res=Integer.MIN_VALUE;
+        for(int i=0;i<accounts.length;i++)
+        {
+            sum=0;
+            for(int j=0;j<accounts[i].length;j++)
+            {
+                sum+=accounts[i][j];
+            }
+                res=Math.max(res,sum);
+        }
+        return res;
+    }
+}
